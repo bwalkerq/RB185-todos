5df5ac5ac378c807255e2529b5e8b662fc2f2dd7
Benjamin Walker <benjamin.q.walker@gmail.com>
2024-01-09 11:14:51 -0600

    pushing down operations (counting, specfically) to the database from the main app

--- database_persistence.rb
+++ database_persistence.rb
@@ -16,24 +16,36 @@ class DatabasePersistence
   end
 
   def find_list(id)
-    sql = 'SELECT * FROM lists where id = $1'
+    sql = <<~SQL_statement
+      SELECT lists.*,
+      count(t.id) AS todos_count,
+                     count(NULLIF(t.completed, true)) as todos_remaining_count
+      FROM lists
+      LEFT JOIN todos t on lists.id = t.list_id
+      WHERE lists.id = $1
+      GROUP BY lists.id
+      ORDER BY lists.name;
+    SQL_statement
     result = query(sql, id)
 
-    tuple = result.first
-
-    list_id = tuple["id"].to_i
-    todos = find_todos_for_list(list_id)
-    {id: tuple["id"], name: tuple["name"], todos: todos}
+    tuple_to_list_hash(result.first)
   end
 
   def all_lists
-    sql = "SELECT * FROM lists"
+    sql = <<~SQL_statement
+      SELECT lists.*,
+      count(t.id) AS todos_count,
+                     count(NULLIF(t.completed, true)) as todos_remaining_count
+      FROM lists
+      LEFT JOIN todos t on lists.id = t.list_id
+      GROUP BY lists.id
+      ORDER BY lists.name;
+    SQL_statement
+
     result = query(sql)
 
     result.map do |tuple|
-      list_id = tuple["id"].to_i
-      todos = find_todos_for_list(list_id)
-      {id: list_id, name: tuple["name"], todos: todos}
+      tuple_to_list_hash(tuple)
     end
   end
 
@@ -76,8 +88,6 @@ class DatabasePersistence
     # end
   end
 
-  private
-
   def find_todos_for_list(list_id)
     todo_sql = "SELECT * FROM todos WHERE list_id = $1"
     todos_result = query(todo_sql, list_id )
@@ -87,5 +97,14 @@ class DatabasePersistence
        name: todo_tuple["name"],
        completed: todo_tuple["completed"] == 't' }
     end
+  end
+
+  private
+
+  def tuple_to_list_hash(tuple)
+    { id: tuple["id"].to_i,
+      name: tuple["name"],
+      todos_count: tuple["todos_count"].to_i,
+      todos_remaining_count: tuple["todos_remaining_count"].to_i }
   end
 end
